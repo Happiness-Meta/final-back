1641bc10ae78337f1ce710c44ae131b829c5cae0
max96312 <k0mp4ny16@gmail.com>
2024-03-06 19:34:48 +0900

    fetch: baseEntity

---
+++ src/main/java/org/happinessmeta/last/common/entity/BaseTimeEntity.java
@@ -0,0 +1,29 @@
+package org.happinessmeta.last.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime lastModifiedAt;
+}
