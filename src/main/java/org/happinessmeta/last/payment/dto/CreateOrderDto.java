4f3dd8b9a5725aa6413fe263b727824cdb378dbd
max96312 <k0mp4ny16@gmail.com>
2024-03-19 18:20:21 +0900

    Fix: int type @NotBlank -> @NotNull 변경

--- src/main/java/org/happinessmeta/last/payment/dto/CreateOrderDto.java
+++ src/main/java/org/happinessmeta/last/payment/dto/CreateOrderDto.java
@@ -1,9 +1,10 @@
 package org.happinessmeta.last.payment.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CreateOrderDto(
-        @NotBlank(message = "상품 가격은 필수 값입니다.")
+        @NotNull(message = "상품 가격은 필수 값입니다.")
         int itemPrice,
         @NotBlank(message = "상품명은 필수 값입니다.")
         String itemName
