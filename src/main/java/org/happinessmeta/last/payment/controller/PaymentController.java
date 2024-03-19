81bc0525b1c515a9390ef63a6e73a13896db1b59
max96312 <k0mp4ny16@gmail.com>
2024-03-19 18:24:15 +0900

    feat: payment controller & service 결제 검증 기능 완료

--- src/main/java/org/happinessmeta/last/payment/controller/PaymentController.java
+++ src/main/java/org/happinessmeta/last/payment/controller/PaymentController.java
@@ -1,23 +1,39 @@
 package org.happinessmeta.last.payment.controller;
 
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.happinessmeta.last.payment.dto.PaymentCallbackRequest;
 import org.happinessmeta.last.payment.service.PaymentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/api/v1/payment")
+@Tag(name = "결제 기능", description = "Order API")
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class PaymentController {
     private final PaymentService paymentService;
 
-    @GetMapping("/payment/{id}")
+    @Operation(summary = "test", description = "")
+    @GetMapping("/{id}")
     public String payment(){
         return "payment";
     }
 
-    @PostMapping("/payment/{id}")
-    public String validationPayment(){
-        return "validationPayment";
+    @Operation(summary = "결제 완료 후 서버에 결제 검증 요청", description = "")
+    @ResponseBody
+    @PostMapping
+    public ResponseEntity<IamportResponse<Payment>> validationPayment(@RequestBody PaymentCallbackRequest request) {
+        IamportResponse<Payment> iamportResponse = paymentService.paymentByCallback(request);
+
+        log.info("결제 응답={}", iamportResponse.getResponse().toString());
+
+        return new ResponseEntity<>(iamportResponse, HttpStatus.OK);
     }
 }
