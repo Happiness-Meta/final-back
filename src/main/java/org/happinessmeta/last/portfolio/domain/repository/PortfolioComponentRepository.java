61c59beffa3aed1efb56949b9b2fe7ce938ecdfc
Junsu Park <pksjmh5295@gmail.com>
2024-03-19 14:41:17 +0900

    Feat: 포트폴리오 컴포넌트 조회

--- src/main/java/org/happinessmeta/last/portfolio/domain/repository/PortfolioComponentRepository.java
+++ src/main/java/org/happinessmeta/last/portfolio/domain/repository/PortfolioComponentRepository.java
@@ -1,7 +1,15 @@
 package org.happinessmeta.last.portfolio.domain.repository;
 
 import org.happinessmeta.last.portfolio.domain.entity.PortfolioComponent;
+import org.happinessmeta.last.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PortfolioComponentRepository extends JpaRepository<PortfolioComponent, Long> {
+
+    @Query("select p from PortfolioComponent p join fetch p.user u where u = :user and p.id IS NOT NULL")
+    List<PortfolioComponent> findAllByUser(@Param("user") User user);
 }
