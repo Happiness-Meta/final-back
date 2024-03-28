6b6b2ecdbfcfdf84cee1f8ee9c9859107a1d621c
Jason, Yang <didwotjs120@gmail.com>
2024-03-28 10:45:42 +0900

    HotFix: 사용자 정보 업데이트 안되는 버그 해결(context 상에서는 변경 사항이 반영 되지만, db에는 반영이 안. @Transactional 어노테이션으로 db에 commit)

--- src/main/java/org/happinessmeta/last/user/service/UserService.java
+++ src/main/java/org/happinessmeta/last/user/service/UserService.java
@@ -1,6 +1,7 @@
 package org.happinessmeta.last.user.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.happinessmeta.last.common.exception.UserNameDuplicatedException;
 import org.happinessmeta.last.common.exception.UserNotFoundException;
 import org.happinessmeta.last.user.domain.User;
@@ -9,16 +10,20 @@ import org.happinessmeta.last.user.dto.UserUpdateRequest;
 import org.happinessmeta.last.user.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static java.util.stream.Collectors.*;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
+
+
     public UserResponse findUserByEmail(String email) {
         return UserResponse.toDto(repository.findByEmail(email).orElseThrow(UserNotFoundException::new));
     }
@@ -30,6 +35,7 @@ public class UserService {
     // todo: 사용자별로 개인 정보 업데이트 메서드를 구현하지 않았을 때 발생할 수 있는 문제점 파악하기.
     // todo: 비밀번호를 바꾸지 않은 상태에서 put 요청을 보낸다면? 분명히 null이 받아질 것이다.
     // todo: 변경된 부분만 바꾸고 싶을 때는? dirty check 하는 방법은?
+    @Transactional
     public UserResponse updateUser(String email, UserUpdateRequest request) {
         User findUser = repository.findByEmail(email)
                 .orElseThrow(UserNotFoundException::new);
